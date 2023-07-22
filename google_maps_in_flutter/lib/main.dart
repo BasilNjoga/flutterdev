17f93bf216428b038c1d3ed1f9f5d1b3cd74d028
Basil Njoga <Basilnjoga@gmail.com>
2023-07-22 15:40:09 +0300

    A google apps api application to view maps between two locations and map the journey and a namer app out of the flutter website application

---
+++ google_maps_in_flutter/lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
+
+  final LatLng sourceLocation =
+      const LatLng(-1.0909771979474097, 37.01120191308141);
+  final LatLng destination =
+      const LatLng(-1.1248462242034525, 37.00324046871465);
+  final googleApiKey = "AIzaSyDA5GRgkanc2LJyzcmC62cZ9j82QgVjyKg";
+
+  List<LatLng> polylineCoordinates = [];
+
+  void getPolyPoints() async {
+    PolylinePoints polylinePoints = PolylinePoints();
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(
+        googleApiKey,
+        PointLatLng(sourceLocation.latitude, sourceLocation.longitude),
+        PointLatLng(destination.latitude, destination.longitude));
+
+    if (result.points.isNotEmpty) {
+      result.points.forEach(
+        (PointLatLng point) => polylineCoordinates.add(
+          LatLng(point.latitude, point.longitude),
+        ),
+      );
+      setState(() {});
+    }
+  }
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
+  void initState() {
+    getPolyPoints();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.green[700],
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Maps Sample App'),
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: sourceLocation,
+            zoom: 14.5,
+          ),
+          polylines: {
+            const Polyline(
+              polylineId: PolylineId("route"),
+              points: polylineCoordinates,
+            ),
+          },
+          markers: {
+            Marker(
+                markerId: const MarkerId("source"), position: sourceLocation),
+            Marker(
+                markerId: const MarkerId("destination"), position: destination)
+          },
+        ),
+      ),
+    );
+  }
+}
