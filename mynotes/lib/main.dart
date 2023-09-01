f4551acfed3d42e7108209d43ff5b626c8773392
Basil Njoga <Basilnjoga@gmail.com>
2023-09-01 23:20:51 +0300

    created homepage and added email verifications functionality

--- mynotes/lib/main.dart
+++ mynotes/lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:mynotes/views/login_view.dart';
-import 'firebase_options.dart';
+import 'package:mynotes/firebase_options.dart';
+
+
 
 void main() {
   runApp(MaterialApp(
@@ -12,42 +13,18 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ));
 }
 
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
-}
-
-class _RegisterViewState extends State<RegisterView> {
-
-  
-  late final TextEditingController _email;
-  late final TextEditingController _password;
-
-  @override
-  void initState() {
-    _email = TextEditingController();
-    _password =  TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
+ Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Register"),
+        title: const Text("Home"),
         ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
@@ -56,55 +33,14 @@ class _RegisterViewState extends State<RegisterView> {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return Column(
-          children: [
-            TextField(
-              controller: _email,
-              enableSuggestions: false,
-              autocorrect: false,
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                hintText: 'Enter your email',
-              ),
-            ),
-            TextField(
-              controller: _password,
-              obscureText: true,
-              enableSuggestions: false,
-              autocorrect: false,
-              decoration: const InputDecoration(
-                hintText: 'Enter your password here',
-              )
-            ),
-            TextButton(
-              onPressed: () async {
-                final email = _email.text;
-                final password = _password.text;
-                try {
-                  final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
-                  email: email, password: password,
-                  );
-                  print(userCredential);
-                } on FirebaseAuthException catch (e) {
-                  if (e.code == 'weak-password') {
-                    print("password should be atleast 6 characters");
-                  }
-                  else if (e.code == 'email-already-in-use') {
-                    print("email is already registered");
-                  } 
-                  else if (e.code == 'invalid-email'){
-                    print("Please enter a valid email address");
-                  }
-                  else {
-                    print(e.code);
-                  }
-                }
-                
-              },
-              child: const Text('Register'),
-              ),
-          ],
-        );
+            
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print("user is verified");
+              } else {
+                print("User is not a verified user");
+              }
+              return const Text('Done');
             default:
               return const Text('Loading...');
           }
@@ -114,4 +50,3 @@ class _RegisterViewState extends State<RegisterView> {
       ),);
   }
 }
-
